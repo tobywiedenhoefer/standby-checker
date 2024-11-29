56064a9a96ae6e068b21d93dbaed7b994954a71a
tobywiedenhoefer <toby.wiedenh@gmail.com>
2024-11-29 17:00:21 -0600

    Added middle shelf to TicketCard component

--- components/TicketCard.tsx
+++ components/TicketCard.tsx
@@ -1,12 +1,15 @@
 import {View, ViewProps} from "react-native"
 
+import Feather from "@expo/vector-icons/Feather"
+
 import Ticket from "@/types/Ticket.type"
 import Airline from "@/types/Airline.type"
 import Flights from "@/types/Flight.type"
 
 import Label from "@/components/text/Label"
+import P from "@/components/text/P"
 
-import {tripCardStyles} from "@/constants/styling";
+import {tripCardStyles, textStyle} from "@/constants/styling";
 
 type TicketCardProps = {
     ticket: Ticket
@@ -21,7 +24,7 @@ export default function TicketCard(props: TicketCardProps) {
         .map((flight, ind) => flight.destination[Object.keys(props.ticket.flights).length - 1 === ind ? "to" : "from"].country)
         .filter((country, ind, arr) => arr.findIndex(otherCountry => country === otherCountry) === ind)
     return (
-        <View style={[tripCardStyles.shape]}>
+        <View style={[tripCardStyles.shape, props.style]}>
             <View style={[tripCardStyles.topShelf]}>
                 <View style={[tripCardStyles.sections]}>
                     <Label>Layovers</Label>
@@ -34,6 +37,15 @@ export default function TicketCard(props: TicketCardProps) {
                 <View style={[tripCardStyles.sections]}>
                     <Label>Countries</Label>
                     <Label>{countries.length}</Label>
+                </View>
+            </View>
+            <View style={[tripCardStyles.middleShelf]}>
+                <View style={[tripCardStyles.toFromText]}>
+                    <P>{props.ticket.flights[0].destination.from.airportCode}</P>
+                    <View style={{justifyContent: "center", alignItems: "center"}}>
+                        <Feather name={"arrow-right"} size={textStyle.p.fontSize}/>
+                    </View>
+                    <P>{props.ticket.flights[Object.keys(props.ticket.flights).length - 1].destination.to.airportCode}</P>
                 </View>
             </View>
             <View style={[tripCardStyles.bottomShelf]}>
