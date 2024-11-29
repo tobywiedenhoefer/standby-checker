64636cf9b6754c1d9c087c810fc60944686a23e2
tobywiedenhoefer <toby.wiedenh@gmail.com>
2024-11-29 16:59:04 -0600

    Updated ThemedButton to add disabled prop

--- components/ThemedButton.tsx
+++ components/ThemedButton.tsx
@@ -1,17 +1,18 @@
 import {TouchableOpacity, View, ViewProps} from "react-native"
 
-import {buttonStyles} from "@/constants/styling"
+import {buttonStyles, colorScheme} from "@/constants/styling"
 
 import P from "@/components/text/P"
 
 type ThemedButtonProps = {
     label: string,
     variant: "primary" | "secondary" | "tertiary",
-    onPress: () => void
+    onPress: () => void,
+    disabled?: boolean
 } & ViewProps
 export default function ThemedButton(props: ThemedButtonProps) {
     return (
-        <TouchableOpacity onPress={props.onPress} style={[props.style]}>
+        <TouchableOpacity onPress={props.onPress} style={[props.style]} disabled={props.disabled}>
             <View style={[buttonStyles.base, buttonStyles[props.variant]]}>
                 <P>{props.label}</P>
             </View>
